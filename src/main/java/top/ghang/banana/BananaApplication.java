ad6c9193abde74cbd6ac4e360e53e9381189e6da
gaohanghang <1341947277@qq.com>
2018-12-27 15:42:34 +0800

    update

--- src/main/java/top/ghang/banana/BananaApplication.java
+++ src/main/java/top/ghang/banana/BananaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BananaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BananaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BananaApplication.class, args);
+    }
 }
--- src/test/java/top/ghang/banana/BananaApplicationTests.java
+++ src/test/java/top/ghang/banana/BananaApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BananaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
